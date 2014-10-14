cc6b7ddba480f05cf8fe3680b1a3fedb08cff2b0
kawasima <kawasima1016@gmail.com>
2014-10-14 12:33:14 +0900

    Release 0.1.0

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject net.unit8/axebomber-clj "0.1.0-SNAPSHOT"
+(defproject net.unit8/axebomber-clj "0.1.0"
   :description "The generator for MS-Excel grid sheet"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "same as Clojure"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.apache.poi/poi "3.10-FINAL"]
-                 [org.apache.poi/poi-ooxml "3.10-FINAL"]
+                 [org.apache.poi/poi "3.10.1"]
+                 [org.apache.poi/poi-ooxml "3.10.1"]
                  [clj-time "0.8.0"]
                  [hiccup "1.0.5"]]
   :profiles {:dev
